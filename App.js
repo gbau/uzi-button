c7de7ef174af088ed7135b8a620f624ac6052529
Giancarlo Bautista <giancarlo@smartlogic.io>
2017-11-28 10:31:37 -0500

    Add some more uzi ad libs........OOOOOOOOOOOOO

--- App.js
+++ App.js
@@ -11,49 +11,91 @@ import {
 } from 'react-native';
 import Dimensions from 'Dimensions';
 import Sound from 'react-native-sound';
-import yahWav from './yah.wav';
 
 Sound.setCategory('Playback');
-const yah = new Sound('./yah.wav', Sound.MAIN_BUNDLE, error => {});
+const sounds = {
+  yah: new Sound('./yah.wav', Sound.MAIN_BUNDLE, error => {}),
+  ooo: new Sound('./ooo.wav', Sound.MAIN_BUNDLE, error => {}),
+  huh: new Sound('./huh.wav', Sound.MAIN_BUNDLE, error => {}),
+  aye: new Sound('./aye.wav', Sound.MAIN_BUNDLE, error => {}),
+};
+
+const INITIAL_STATE = {
+  pressed: {}
+};
 
 export default class App extends Component<{}> {
-  state = {
-    pressed: false,
+  state = { ...INITIAL_STATE }
+
+  componentWillUnmount() {
+    Object.values(sounds).forEach(sound => sound.release());
   }
 
-  onPress = pressed => () => {
-    this.setState({ pressed }, () => {
-      if (pressed) {
-        yah.stop(() => {
-          yah.play(success => {});
-        });
+  onPressIn = name => () => {
+    const { pressed } = this.state;
+    const sound = sounds[name];
+
+    this.setState({
+      pressed: {
+        ...pressed,
+        [name]: true,
+      }
+    }, () => {
+      sound.stop(() => {
+        sound.play(success => {});
+      });
+    });
+  }
+
+  onPressOut = name => () => {
+    const { pressed } = this.state;
+
+    this.setState({
+      pressed: {
+        ...pressed,
+        [name]: false,
       }
     });
   }
 
-  render() {
+  renderButton(name) {
+    const { pressed } = this.state;
+    const isPressed = pressed[name];
     const buttonStyles = [
       styles.button,
-      this.state.pressed ? styles.buttonPressed : null,
+      isPressed ? styles.buttonPressed : null,
     ];
 
     return (
+      <TouchableWithoutFeedback
+        onPressIn={this.onPressIn(name)}
+        onPressOut={this.onPressOut(name)}
+        key={name}
+      >
+        <View style={buttonStyles}>
+          <Text style={styles.text}>{name}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+
+  render() {
+    const buttons = Object.keys(sounds).map(name => (
+      this.renderButton(name, sounds[name])
+    ));
+
+    return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback
-          onPressIn={this.onPress(true)}
-          onPressOut={this.onPress(false)}
-        >
-          <View style={buttonStyles}>
-            <Text style={styles.text}>UZI</Text>
-          </View>
-        </TouchableWithoutFeedback>
+        <View style={styles.buttons}>
+          {buttons}
+        </View>
       </View>
     );
   }
 }
 
-const { height } = Dimensions.get('window');
-const HEIGHT = height / 3;
+const { height, width } = Dimensions.get('window');
+const HEIGHT = height / 5;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -61,9 +103,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  buttons: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   button: {
-    height: height / 3,
-    width: height / 3,
+    height: HEIGHT,
+    width: HEIGHT,
+    margin: 12,
     borderRadius: HEIGHT / 2,
     backgroundColor: 'green',
     display: 'flex',
